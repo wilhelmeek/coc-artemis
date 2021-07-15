f82bff1aff768f531cf1e8f1194c0e393ec5907d
wilhelm <helm@hey.com>
2021-07-15 17:15:49 +1000

    Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # coc-apollo
-Don't look at this please. It's guaranteed to be embarrassing. 
+
+Apollo GraphQL Extension
+
+## Install
+
+`:CocInstall coc-apollo`
+
+## Keymaps
+
+`nmap <silent> <C-l> <Plug>(coc-apollo-keymap)`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import {
+  commands,
+  CompleteResult,
+  ExtensionContext,
+  listManager,
+  sources,
+  window,
+  workspace,
+} from 'coc.nvim';
+import DemoList from './lists';
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  window.showMessage(`coc-apollo works!`);
+
+  context.subscriptions.push(
+    commands.registerCommand('apollo.Command', async () => {
+      window.showMessage(`apollo Commands works!`);
+    }),
+
+    listManager.registerList(new DemoList(workspace.nvim)),
+
+    sources.createSource({
+      name: 'apollo completion source', // unique id
+      doComplete: async () => {
+        const items = await getCompletionItems();
+        return items;
+      },
+    }),
+
+    workspace.registerKeymap(
+      ['n'],
+      'apollo-keymap',
+      async () => {
+        window.showMessage(`registerKeymap`);
+      },
+      { sync: false }
+    ),
+
+    workspace.registerAutocmd({
+      event: 'InsertLeave',
+      request: true,
+      callback: () => {
+        window.showMessage(`registerAutocmd on InsertLeave`);
+      },
+    })
+  );
+}
+
+async function getCompletionItems(): Promise<CompleteResult> {
+  return {
+    items: [
+      {
+        word: 'TestCompletionItem 1',
+        menu: '[coc-apollo]',
+      },
+      {
+        word: 'TestCompletionItem 2',
+        menu: '[coc-apollo]',
+      },
+    ],
+  };
+}
