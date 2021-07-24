5cf2d22cb793801172627d7cd6befc867560128c
wilhelm <helm@hey.com>
2021-07-24 21:55:33 +1000

    Make It Work

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,104 @@
+import { join } from 'path';
+import { ApolloConfig } from 'apollo-language-server';
 import {
-  commands,
-  CompleteResult,
   ExtensionContext,
-  sources,
+  services,
+  LanguageClient,
+  TransportKind,
   window,
   workspace,
+  ServerOptions,
+  LanguageClientOptions,
 } from 'coc.nvim';
 
-export async function activate(context: ExtensionContext): Promise<void> {
-  window.showMessage(`coc-artemis works!`);
+const { version } = require('../package.json');
 
-  context.subscriptions.push(
-    commands.registerCommand('apollo.Command', async () => {
-      window.showMessage(`apollo Commands works!`);
-    }),
+const artemis = 'coc-artemis';
 
-    sources.createSource({
-      name: 'apollo completion source', // unique id
-      doComplete: async () => {
-        const items = await getCompletionItems();
-        return items;
-      },
-    }),
-
-    workspace.registerKeymap(
-      ['n'],
-      'apollo-keymap',
-      async () => {
-        window.showMessage(`registerKeymap`);
-      },
-      { sync: false }
-    ),
-
-    workspace.registerAutocmd({
-      event: 'InsertLeave',
-      request: true,
-      callback: () => {
-        window.showMessage(`registerAutocmd on InsertLeave`);
-      },
-    })
-  );
+function decorate(m: string): string {
+  return `[${artemis}] ${m}`;
 }
 
-async function getCompletionItems(): Promise<CompleteResult> {
-  return {
-    items: [
-      {
-        word: 'TestCompletionItem 1',
-        menu: '[coc-artemis]',
-      },
-      {
-        word: 'TestCompletionItem 2',
-        menu: '[coc-artemis]',
-      },
-    ],
+export async function activate(context: ExtensionContext): Promise<void> {
+  const serverModule = context.asAbsolutePath(
+    join('node_modules/apollo-language-server/lib', 'server.js')
+  );
+
+  const env = {
+    APOLLO_CLIENT_NAME: artemis,
+    APOLLO_CLIENT_VERSION: version,
+    APOLLO_CLIENT_REFERENCE_ID: version,
+    NODE_TLS_REJECT_UNAUTHORIZED: 0,
   };
+
+  const serverOptions: ServerOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { env },
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { env, execArgv: ['--nolazy', '--inspect=6009'] },
+    },
+  };
+
+  const outputChannel = window.createOutputChannel(artemis);
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      'graphql',
+      'javascript',
+      'typescript',
+      'javascriptreact',
+      'typescriptreact',
+      'vue',
+      'python',
+      'ruby',
+      'dart',
+      'reason',
+      'elixir',
+    ],
+    synchronize: {
+      fileEvents: [
+        workspace.createFileSystemWatcher('**/.env?(.local)'),
+        workspace.createFileSystemWatcher(
+          '**/*.{graphql,js,ts,jsx,tsx,vue,py,rb,dart,re,ex,exs}'
+        ),
+      ],
+    },
+    outputChannel,
+  };
+
+  const client = new LanguageClient(
+    artemis,
+    artemis,
+    serverOptions,
+    clientOptions
+  );
+
+  client.onReady().then(() => {
+    client.onNotification('apollographql/configFilesFound', (resp: string) => {
+      const configs: Array<ApolloConfig> = JSON.parse(resp);
+
+      const schemaNames = new Array<string>();
+      configs.forEach((c) => {
+        if (typeof c.client?.service === 'string') {
+          schemaNames.push(c.client.service);
+        } else if (typeof c.client?.service?.name === 'string') {
+          schemaNames.push(c.client.service.name);
+        }
+      });
+
+      if (schemaNames.length > 0) {
+        const joined = schemaNames.join(', ');
+        window.showMessage(decorate(`found schemas [${joined}]`));
+      } else {
+        window.showWarningMessage(decorate('no schemas found'));
+      }
+    });
+  });
+
+  services.registLanguageClient(client);
 }
